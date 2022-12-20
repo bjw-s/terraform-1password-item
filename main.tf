0b5cc0cfa27593c325f6535872d45de6a12bdf86
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-12-20 14:29:09 +0100

    feat: Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "1.1.4"
+    }
+  }
+}
+
+data "onepassword_vault" "vault" {
+  name = var.vault
+}
+
+data "onepassword_item" "item" {
+  vault = data.onepassword_vault.vault.uuid
+  title = var.item
+}
+
